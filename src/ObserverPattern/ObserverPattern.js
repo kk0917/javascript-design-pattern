52cfa6853c9ca8af7a0f6f15ded4b3ab08e5190b
ko-kamenashi <kamenashi0917@gmail.com>
2020-11-27 20:52:22 +0900

    chapter2-5
    Learn different of observer and pub/sub, implement pub/sub pattern

--- src/ObserverPattern/ObserverPattern.js
+++ src/ObserverPattern/ObserverPattern.js
@@ -207,5 +207,55 @@ let pubsub = {};
   let topics = {},
       subUid = -1;
 
-  // ... TODO: WIP
-})();
+  /**
+   * Publish an events of interest, or broadcasting.
+   * Specify a specific topic name, data to be passed together, etc. by arguments
+   */
+  q.publish = (topic, args) => {
+    if (!topics[topic]) return false;
+
+    let subscribers = topics[topic],
+        length = subscribers ? subscribers.length : 0;
+
+    while (length--) {
+      subscribers[length].func(topic, args);
+    }
+
+    return this;
+  };
+
+  /**
+   * Specify a specific topic name, callback function by arguments.
+   * Callback function execute when topic/event is subscribed.
+   */
+  q.subscribe = (topic, func) => {
+    if (!topics[topic]) topics[topic] = [];
+
+    let token = (++subUid).toString();
+    topics[topic].push({
+      token: token,
+      func: func
+    });
+
+    return token;
+  };
+
+  /**
+   * Stops subscription of specific topic.
+   * Topic tokenize to be able to search.
+   */
+  q.unsubscribe = token => {
+    for (const m in topics) {
+      if (topics[m]) {
+        for (let i = 0, j = topics[m].length; i < j; i++) {
+          if (topics[m][i].token === token) {
+            topics[m].splice(i, 1);
+
+            return token;
+          }
+        }
+      }
+    }
+  };
+
+})(pubsub);
